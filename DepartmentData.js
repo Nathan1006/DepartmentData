b2d32fae2633c158ef61762d78213e305edb9ec5
Nathan1006 <nrpetit@outlook.com>
2021-09-09 21:23:34 -0400

    Data

--- DepartmentData.js
+++ DepartmentData.js
@@ -1,1 +1,21 @@
-// Department Project
+/* Department Project Legend:
+
+    Departents: i: 0: Department ID
+    Departents: i: 1: Department Name
+
+    EmployeeDepartments: i: 0: Employee ID
+    EmployeeDepartments: i: 1: Employee Department
+    EmployeeDepartments: i: 2: Initial Date in Department
+    EmployeeDepartments: i: 3: Last Date in Department
+
+    Employees: i: 0: Employee ID
+    Employees: i: 1: Employee Date of Birth
+    Employees: i: 2: Employee Name
+    Employees: i: 3: Employee Date of Employment
+
+    Salaries: i: 0: Employee ID
+    Salaries: i: 0: Earnings
+    Salaries: i: 0: Initial Date of Payment Period
+    Salaries: i: 0: Ending Date of Payment Period
+*/
+
