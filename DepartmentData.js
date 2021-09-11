a61b5904112b925fc19bb648ada5a26b74be7fd0
Nathan1006 <nrpetit@outlook.com>
2021-09-11 14:04:24 -0400

    data

--- DepartmentData.js
+++ DepartmentData.js
@@ -10,12 +10,16 @@
 
     Employees: i: 0: Employee ID
     Employees: i: 1: Employee Date of Birth
-    Employees: i: 2: Employee Name
-    Employees: i: 3: Employee Date of Employment
+    Employees: i: 2: Employee First Name
+    Employees: i: 3: Employee last NAme
+    Employees: i: 4: Employee Sex (male or female)
+    Employees: i: 5: Employee Date of Employment
 
     Salaries: i: 0: Employee ID
-    Salaries: i: 0: Earnings
-    Salaries: i: 0: Initial Date of Payment Period
-    Salaries: i: 0: Ending Date of Payment Period
+    Salaries: i: 1: Earnings
+    Salaries: i: 2: Initial Date of Payment Period
+    Salaries: i: 3: Ending Date of Payment Period
 */
+
+
 
