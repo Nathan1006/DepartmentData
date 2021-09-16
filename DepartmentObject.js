a448798758adc5cc17c63ff791089d85ac3baff3
Nathan1006 <nrpetit@outlook.com>
2021-09-16 18:04:22 -0400

    Final Javascript Challenges

--- DepartmentObject.js
+++ DepartmentObject.js
@@ -14,78 +14,101 @@ var empDeps = parseData('EmployeeDepartments')
 var emps = parseData('Employees')
 var salaries = parseData('Salaries')
 
-function empObjects(){
-    let employee = []
-    for (let i = 0; i < 35; i++){
-        let info = {}
-        info.id = parseInt(emps[i][0])
-        info.name = emps[i][2] + ' ' + emps[i][3]
-        switch (emps[i][4]){
-            case 'M':
-                info.gender = 'Male'
-                break
-            case 'F':
-                info.gender = 'Female'
-                break
-        }
-        info.dateOfBirth = emps[i][1]
-        info.startYear = emps[i][5][0] + emps[i][5][1] + emps[i][5][2] + emps[i][5][3]
-        // Need help
-        for (let o = 0; o < salaries.length; o++){
-            if (salaries[o][0] == salaries[o][0]){
-                if (o == salaries.length - 1){
-                    break
-                }
-                info.endYear = salaries[o][3][0] + salaries[o][3][1] + salaries[o][3][2] + salaries[o][3][3]
-            }
-        }
-
-        employee.push(info)
+class Employee {
+    constructor (id, name, gender, dateOfBirth, startYear){
+        this.id = id
+        this.name = name
+        this.gender = gender 
+        this.dateOfBirth = dateOfBirth 
+        this.startYear = startYear
+        this.lastYear = null
+        this.department = 'NA'
+        this.salary = 0
     }
-    console.log(employee)
 }
 
-function depObjects(){
-    let department = []
-    for (let i = 0; i < deps.length; i++){
-        let info = {}
-        let numEmps = 0
-        let male = 0
-        let female = 0
-        let sal = 0
-        info.id = deps[i][0]
-        info.name = deps[i][1]
+class Department {
+    constructor (id, name){
+        this.id = id
+        this.name = name 
+        this.numberOfEmps = 0
+        this.genders = {Males: 0, Females: 0}
+        this.totalSal = 0
+    }
+}
+
+let employeeList = []
+let departmentList = []
+
+function empObjects(){
+    for (let i = 0; i < emps.length; i++){
+        let emp = new Employee(emps[i][0], emps[i][2] + ' ' + emps[i][3], emps[i][4], emps[i][1], emps[i][5])
+        employeeList.push(emp)
+    }
+    for (let e = 0; e < employeeList.length; e++){
         for (let o = 0; o < empDeps.length; o++){
             if (o != empDeps.length - 1){
                 if (empDeps[o][0] == empDeps[o + 1][0]){
                     o++
                 }
             }
-            if (empDeps[o][1] == deps[i][0]){
-                numEmps++
-            }
-            for (let g = 0; g < emps.length; g++){
-                if (emps[g][0] == empDeps[o][0]){
-                    if (empDeps[o][1] == deps[i][0]){
-                        switch (emps[g][4]){
-                            case 'M':
-                                male++
-                                break
-                            case 'F':
-                                female++
-                                break
-                        }
+            if (employeeList[e].id == empDeps[o][0]){
+                for (let d = 0; d < deps.length; d++){
+                    if (empDeps[o][1] == deps[d][0]){
+                        employeeList[e].department = deps[d][1]
                     }
                 }
+                employeeList[e].lastYear = empDeps[o][3]
             }
         }
-        info.numberOfEmployees = parseInt(numEmps)
-        info.genders = {males: parseInt(male), females: parseInt(female)}
-        info.totalSalary = parseInt(sal)
-
-        department.push(info)
+        for (let s = 0; s < salaries.length; s++){
+            while (salaries[s][0] == salaries[s + 1][0]){
+                if (s != salaries.length - 1){
+                    s++
+                }
+                if (s == salaries.length - 1){
+                    break
+                }
+            }
+            if (salaries[s][0] == employeeList[e].id){
+                employeeList[e].salary = parseInt(salaries[s][1])
+            }
+        }
     }
-    console.log(department)
+
+    console.log(employeeList)
+}
+
+function depObjects(){
+    for (let i = 0; i < deps.length; i++){
+        let dep = new Department(deps[i][0], deps[i][1])
+        departmentList.push(dep)
+    }
+    for (let d = 0; d < departmentList.length; d++){
+        let numEmps = 0
+        let numMales = 0
+        let numFemales = 0
+        let sal = 0
+        for (let e = 0; e < employeeList.length; e++){
+            if (departmentList[d].name == employeeList[e].department){
+                numEmps++
+                switch (employeeList[e].gender){
+                    case 'M':
+                        numMales++
+                        break
+                    case 'F':
+                        numFemales++
+                        break
+                }
+                sal += employeeList[e].salary
+            }
+        }
+        departmentList[d].numberOfEmps = numEmps
+        departmentList[d].genders = {Males: numMales, Females: numFemales}
+        departmentList[d].totalSal = sal
+    }
+
+    console.log(departmentList)
 }
 
 empObjects()
